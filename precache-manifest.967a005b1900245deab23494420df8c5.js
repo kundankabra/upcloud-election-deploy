7ceb59795e3306e9fb1a9b645fd0bfbf14ab0368
kundankabra <kundan.21810729@viit.ac.in>
2020-11-18 17:05:30 +0530

    Updates

---
+++ precache-manifest.967a005b1900245deab23494420df8c5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2511e778a303e85e8eccc31ac31f30f",
+    "url": "/upcloud-mbs-deploy/index.html"
+  },
+  {
+    "revision": "1357fdb08bd7a227670b",
+    "url": "/upcloud-mbs-deploy/static/css/2.63b0c82a.chunk.css"
+  },
+  {
+    "revision": "0df56f0688b5f50572d2",
+    "url": "/upcloud-mbs-deploy/static/css/main.7e076407.chunk.css"
+  },
+  {
+    "revision": "1357fdb08bd7a227670b",
+    "url": "/upcloud-mbs-deploy/static/js/2.c1da79eb.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/upcloud-mbs-deploy/static/js/2.c1da79eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df56f0688b5f50572d2",
+    "url": "/upcloud-mbs-deploy/static/js/main.d80d1266.chunk.js"
+  },
+  {
+    "revision": "dd31fd1964cab58192fb",
+    "url": "/upcloud-mbs-deploy/static/js/runtime-main.5a62e5d0.js"
+  },
+  {
+    "revision": "d1b9f22622ea61fea5e1b53c5b198e2c",
+    "url": "/upcloud-mbs-deploy/static/media/Application.d1b9f226.svg"
+  },
+  {
+    "revision": "4ea6e07ed73a3e0ff983e6f729baecfe",
+    "url": "/upcloud-mbs-deploy/static/media/Group294.4ea6e07e.svg"
+  },
+  {
+    "revision": "1d4a1915eb0f6798bc48d56f555e40d2",
+    "url": "/upcloud-mbs-deploy/static/media/Hero.1d4a1915.svg"
+  },
+  {
+    "revision": "1a1cc404d3163e742615002733313dc2",
+    "url": "/upcloud-mbs-deploy/static/media/Information.1a1cc404.svg"
+  },
+  {
+    "revision": "be5beb9a750ebf66e551243a2c066b5e",
+    "url": "/upcloud-mbs-deploy/static/media/image2.be5beb9a.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/upcloud-mbs-deploy/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/upcloud-mbs-deploy/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/upcloud-mbs-deploy/static/media/revicons.2feb69cc.eot"
+  }
+]);
